1e94fa39fa009a94fa9fc96c7093916f1ca2c046
milanlajda <milanlajda1@gmail.com>
2016-08-24 05:45:19 +0200

    need to modify for it to print the recurring prime factors

--- prime_factorization.py
+++ prime_factorization.py
@@ -2,8 +2,7 @@
 def prime_factorization(x):
     primefactors=[]
     for i in range (2, x):
-        blabla = x%i
-        if primenum_check(i) == True and blabla == 0:
+        if primenum_check(i) == True and x%i == 0:
             primefactors.append(i)
     print primefactors
 
@@ -11,12 +10,11 @@ def prime_factorization(x):
 def primenum_check(x):
     counter=0
     for i in range (1, x):
-        remainder = x%i
-        if remainder == 0:
+        if x%i == 0:
             counter=counter+1
     if counter > 1:
         return False
     else:
         return True
 
-prime_factorization(100)
+prime_factorization(120)
